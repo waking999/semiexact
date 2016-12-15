4f64d92c65c9fe1096676330f94814d3a491e61b
waking999 <waking.wang@gmail.com>
2016-12-15 17:53:50 +0930

    add MSC4 part by part - 2
    add deleteSet and testDeleteSet

--- src/test/java/au/edu/cdu/semiexact/io/FileOperationTest.java
+++ src/test/java/au/edu/cdu/semiexact/io/FileOperationTest.java
@@ -5,24 +5,26 @@ import java.nio.file.NoSuchFileException;
 
 import org.junit.Test;
 
+import au.edu.cdu.semiexact.util.GlobalVariable;
+
 public class FileOperationTest {
 	@Test(expected = IOException.class)
 	public void testRetriveProblemInfoByEdgePairIOExp() throws IOException {
 		String filePath = ""; 
-		FileOperation.retriveProblemInfoByEdgePair(filePath); 
+		new FileOperation<Integer,Integer>().retriveProblemInfoByEdgePair(filePath); 
 	}
 	
 	@Test(expected = NoSuchFileException.class)
 	public void testRetriveProblemInfoByEdgePairFoundExp() throws IOException {
 		String filePath = "/Users/kwang/Documents/git/semiexact/src/test/resources/sample.txt1"; 
-		FileOperation.retriveProblemInfoByEdgePair(filePath); 
+		new FileOperation<Integer,Integer>().retriveProblemInfoByEdgePair(filePath); 
 	}
 	
 	@Test 
 	public void testRetriveProblemInfoByEdgePairNormal() throws IOException {
 		String filePath = "/Users/kwang/Documents/git/semiexact/src/test/resources/sample.txt"; 
-		GlobalVariable gv=FileOperation.retriveProblemInfoByEdgePair(filePath); 
+		GlobalVariable<Integer,Integer> gv=new FileOperation<Integer,Integer>().retriveProblemInfoByEdgePair(filePath); 
 		System.out.println(gv);
-		gv.getEal();
+		 
 	}
 }
